ef754b853077a66f2a4de787938399f36db2dc46
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-10 08:20:26 +0300

    create products adapter in app.

---
+++ app/src/main/java/com/route/products_listtask_implementation/ProductsAdapter.kt
@@ -0,0 +1,23 @@
+package com.route.products_listtask_implementation
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductsAdapter : RecyclerView.Adapter<ProductsAdapter.ProductsViewHolder>(){
+
+    class ProductsViewHolder() : RecyclerView.ViewHolder(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ProductsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
