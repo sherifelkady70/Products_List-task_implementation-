27957a5664ae4e6fafc0ecf0cdeb5b1986c8f485
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-10 09:35:24 +0300

    create web service interface to make request to get products.

--- data/src/main/java/com/route/data/api/WebService.kt
+++ data/src/main/java/com/route/data/api/WebService.kt
@@ -1,4 +1,10 @@
 package com.route.data.api
 
+import com.route.data.api.model.ProductResponse
+import com.route.domain.model.ProductsItem
+import retrofit2.http.GET
+
 interface WebService {
+    @GET("/products")
+    suspend fun getProducts() : ProductResponse<List<ProductsItem?>?>
 }
