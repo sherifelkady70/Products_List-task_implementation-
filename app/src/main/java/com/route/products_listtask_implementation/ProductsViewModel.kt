e58cea80ff280c45b67921c448b0a771acc7aa4b
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-11 09:47:04 +0300

    create fun to extract message from response to display it to user.

--- app/src/main/java/com/route/products_listtask_implementation/ProductsViewModel.kt
+++ app/src/main/java/com/route/products_listtask_implementation/ProductsViewModel.kt
@@ -1,7 +1,10 @@
 package com.route.products_listtask_implementation
 
 import androidx.lifecycle.ViewModel
+import com.route.domain.common.InternetConnection
 import com.route.domain.use_case.ProductsUseCase
+import com.route.domain.utils.Resource
+import com.route.products_listtask_implementation.utils.ViewMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,4 +12,32 @@ import javax.inject.Inject
 class ProductsViewModel @Inject constructor(
     private val usecase : ProductsUseCase
 ): ViewModel() {
+
+
+
+    fun <T>extractViewMessage(resource : Resource<T>) : ViewMessage? {
+        return when(resource) {
+            is Resource.Fail -> {
+                when(resource.error){
+                    is InternetConnection -> {
+                        ViewMessage(
+                            resource.error.message
+                        )
+                    }
+                    else ->{
+                        ViewMessage(
+                            resource.error.localizedMessage
+                        )
+                    }
+                }
+            }
+            is Resource.ServerError -> {
+                ViewMessage(
+                    resource.message.serverError
+                )
+            }
+            else -> null
+        }
+    }
+
 }
