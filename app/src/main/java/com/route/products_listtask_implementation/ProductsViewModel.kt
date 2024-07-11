dabc3b6578ffdc36e4a65d3c4815776075d72794
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-11 08:10:38 +0300

    change in application class. create view model. run.

--- app/src/main/java/com/route/products_listtask_implementation/ProductsViewModel.kt
+++ app/src/main/java/com/route/products_listtask_implementation/ProductsViewModel.kt
@@ -1,8 +1,12 @@
 package com.route.products_listtask_implementation
 
 import androidx.lifecycle.ViewModel
+import com.route.domain.use_case.ProductsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class ProductsViewModel : ViewModel() {
+class ProductsViewModel @Inject constructor(
+    private val usecase : ProductsUseCase
+): ViewModel() {
 }
