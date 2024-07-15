fb0aa1a90d6297abcbcc4154c34c4fe403d24939
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-15 13:02:14 +0300

    create class  ProductsOnlineDataSourceImplTest for testing datasource .

--- data/src/test/java/com/route/data/data_source/implementation/ProductsOnlineDataSourceImplTest.kt
+++ data/src/test/java/com/route/data/data_source/implementation/ProductsOnlineDataSourceImplTest.kt
@@ -1,5 +1,22 @@
 package com.route.data.data_source.implementation
 
-class ProductsOnlineDataSourceImplTest{
+import com.route.data.api.ProductsWebService
+import com.route.data.data_source.contract.ProductsOnlineDataSource
+import io.mockk.MockK
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
 
+class ProductsOnlineDataSourceImplTest{
+    lateinit var datasource : ProductsOnlineDataSource
+    val webService = mockk<ProductsWebService>()
+
+    @Before
+    fun setup(){
+        datasource = ProductsOnlineDataSourceImpl(webService)
+    }
+    @Test
+    fun `get list of products when call` (){
+
+    }
 }
