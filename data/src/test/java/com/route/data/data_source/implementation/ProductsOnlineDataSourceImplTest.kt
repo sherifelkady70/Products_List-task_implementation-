64ee763d34c905ec089d7587ed97e6b4094809dc
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-15 13:10:50 +0300

    create class  ProductsOnlineDataSourceImplTest for testing datasource. make fake object for products web service. make fake behavior for data returned from web service.

--- data/src/test/java/com/route/data/data_source/implementation/ProductsOnlineDataSourceImplTest.kt
+++ data/src/test/java/com/route/data/data_source/implementation/ProductsOnlineDataSourceImplTest.kt
@@ -2,13 +2,16 @@ package com.route.data.data_source.implementation
 
 import com.route.data.api.ProductsWebService
 import com.route.data.data_source.contract.ProductsOnlineDataSource
-import io.mockk.MockK
+import com.route.data.api.models.ProductsItem
+import com.route.data.api.models.ProductsResponse
+import io.mockk.coEvery
 import io.mockk.mockk
 import org.junit.Before
 import org.junit.Test
 
 class ProductsOnlineDataSourceImplTest{
     lateinit var datasource : ProductsOnlineDataSource
+    //fake object
     val webService = mockk<ProductsWebService>()
 
     @Before
@@ -16,7 +19,10 @@ class ProductsOnlineDataSourceImplTest{
         datasource = ProductsOnlineDataSourceImpl(webService)
     }
     @Test
-    fun `get list of products when call` (){
-
+    fun `get list of products when call` () {
+        val productsResponse = ProductsResponse()
+        //fake behavior
+        val response = coEvery { webService.getProducts()} returns productsResponse
+        datasource.getProducts()
     }
 }
